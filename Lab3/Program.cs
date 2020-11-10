cb07809036eaa85e650ac80c8c5462dcdf5f69bf
Nikita Kovalenko <nikkv.ottf@gmail.com>
2020-11-11 01:21:41 +0200

    added lab3 w/o comments but working

---
+++ Lab3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!double.TryParse(Console.ReadLine(), out double a))
+            {
+                return;
+            }
+
+            double x = a;
+            double prevX;
+
+            do
+            {
+                Console.WriteLine(x);
+                prevX = x;
+                x = prevX * 4 / 5 + a / (5 * Math.Pow(prevX, 4));
+            } while (Math.Abs(x - prevX) > 0.0001);
+
+            Console.WriteLine($"{x}");
+
+            Console.ReadKey();
+        }
+    }
+}
