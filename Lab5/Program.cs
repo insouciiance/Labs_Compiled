b41923f16f949956fbb2a861c2b47127632a5cd9
Nikita Kovalenko <nikkv.ottf@gmail.com>
2020-12-02 14:25:21 +0200

    done lab 5 + report / ~done lab 6

---
+++ Lab5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter n: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int n) || n <= 0) // ввод числа n с клавиатуры и его валидация
+            {
+                return;
+            }
+
+            for (int c = 1; c <= n; c++)
+            {
+                for (int b = 1; b <= c; b++)
+                {
+                    for (int a = 1; a <= b; a++)
+                    {
+                        if (c * c == a * a + b * b) // проверка на то, является ли текущая тройка чисел a, b, c пифагоровой
+                        {
+                            Console.WriteLine($"a={a} b={b} c={c}");
+                        }
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()?.Split(' ').Select(int.Parse).ToArray() ?? new int[0];
+
+            bool result = ContainsPalindromes(array);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+
+        private static bool ContainsPalindromes(int[] arr)
+        {
+            bool contains = false;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (IsPalindrome(arr[i]))
+                {
+                    contains = true;
+                }
+            }
+
+            return contains;
+
+            bool IsPalindrome(int n)
+            {
+                bool isPalindrome = true;
+
+                int digitsCount = (int)Math.Log10(n) + 1;
+
+                for (int i = 0; i < digitsCount / 2; i++)
+                {
+                    int firstDigitToCompare = n / (int)Math.Pow(10, digitsCount - i - 1) % 10;
+                    int secondDigitToCompare = n / (int)Math.Pow(10, i) % 10;
+
+                    if (firstDigitToCompare != secondDigitToCompare)
+                    {
+                        isPalindrome = false;
+                    }
+                }
+
+                return isPalindrome;
+            }
+        }
+    }
+}
