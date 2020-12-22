3807c779c7cce033ac9631f2c8581defff86899d
Nikita Kovalenko <nikkv.ottf@gmail.com>
2020-12-23 01:45:42 +0200

    added the report (both docx and pdf)

--- Lab9/StringHelper.cs
+++ Lab9/StringHelper.cs
@@ -1,9 +1,17 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Text
 {
     public static class StringHelper
     {
-        public static int SplitWordsCount(string input) => input.Split(';').Skip(1).SkipLast(1).Count(s => s.Trim() != "");
+        public static int SplitWordsCount(string input)
+        {
+            string[] splitInput = input.Split(';');
+            IEnumerable<string> separateStrings = splitInput.Skip(1).SkipLast(1);
+            int wordsCount = separateStrings.Count(s => s.Trim() != ""); 
+
+            return wordsCount;
+        }
     }
 }
